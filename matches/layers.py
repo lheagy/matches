1a41c0e6b01a08552dc166ac81e1afebbd094eae
Lindsey Heagy <lindseyheagy@gmail.com>
2019-05-09 18:37:27 -0700

    wip dense net

--- matches/layers.py
+++ matches/layers.py
@@ -9,23 +9,38 @@ class BaseLayer(nn.Module):
     _default_activation = "relu"
 
     @property
-    def shape(self):
-        return getattr(self, "_shape", None)
+    def input_shape(self):
+        return getattr(self, "_input_shape", None)
 
-    @shape.setter
-    def shape(self, val):
+    @input_shape.setter
+    def input_shape(self, val):
         """
         Shape of the layer
         """
+        self._validate_shape(val)
+        self._shape = val
+
+    @property
+    def output_shape(self):
+        return getattr(self, "_output_shape", None)
+
+    @output_shape.setter
+    def output_shape(self, val):
+        """
+        Shape of the layer
+        """
+        self._validate_shape(val)
+        self._shape = val
+
+    def _validate_shape(self, val)
         if not isinstance(val, tuple):
             try:
                 val = tuple(val)
             except:
                 raise Exception(
-                    f"Type of shape {type(shape)} is not recognized. "
+                    f"Type of shape {type(val)} is not recognized. "
                     "Shape should be a tuple"
                 )
-        self._shape = val
 
     @property
     def activation(self):
@@ -43,9 +58,11 @@ class BaseLayer(nn.Module):
            )
         self._activation = val
 
-    def __init__(self, layer=None, **kwargs):
+    def __init__(self, input_shape=None, output_shape=None, layer=None, **kwargs):
         super(BaseLayer, self).__init__()
         set_kwargs(self, **kwargs)
+        self.input_shape = input_shape
+        self.output_shape = output_shape
         self.layer = layer
 
     def forward(self, x):
@@ -56,13 +73,10 @@ class DenseLayer(BaseLayer):
     A dense layer followed by an activation function
     """
 
-    def __init__(self, shape=None, **kwargs):
-        super(DenseLayer, self).__init__()
-        self.shape = shape
-        set_kwargs(self, **kwargs)
-        self.layer = nn.Linear(self.shape[0], self.shape[1])
+    def __init__(self, input_shape=None, output_shape=None, **kwargs):
+        layer = nn.Linear(input_shape, output_shape)
+        super(DenseLayer, self).__init__(input_shape, output_shape)
 
-    def forward(self, x):
-        return getattr(torch, self.activation)(self.layer(x))
+
 
 
